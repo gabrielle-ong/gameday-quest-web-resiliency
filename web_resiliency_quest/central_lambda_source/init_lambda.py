9d926f3ae25f04f025d209b7410f20250fc21026
Ong <onggabri@3c06304e31da.ant.amazon.com>
2022-09-30 12:31:40 +0800

    text edits

--- web_resiliency_quest/central_lambda_source/init_lambda.py
+++ web_resiliency_quest/central_lambda_source/init_lambda.py
@@ -189,13 +189,14 @@ def lambda_handler(event, context):
     )
 
     # TASK 4
-    image_url_task4 = ui_utils.generate_signed_or_open_url(ASSETS_BUCKET, f"{ASSETS_BUCKET_PREFIX}sample_log.png",signed_duration=86400)
+    # note: Replaced log image with code block if images dont work. If adding back, remember to change below - Task4_Value.format(image_url_task4)
+    # image_url_task4 = ui_utils.generate_signed_or_open_url(ASSETS_BUCKET, f"{ASSETS_BUCKET_PREFIX}sample_log.png",signed_duration=86400)
     quests_api_client.post_output(
         team_id=team_id,
         quest_id=QUEST_ID,
         key=output_const.TASK4_KEY,
         label=output_const.TASK4_LABEL,
-        value=output_const.TASK4_VALUE.format(image_url_task4),
+        value=output_const.TASK4_VALUE,
         dashboard_index=output_const.TASK4_INDEX,
         markdown=output_const.TASK4_MARKDOWN,
     )
--- web_resiliency_quest/central_lambda_source/output_const.py
+++ web_resiliency_quest/central_lambda_source/output_const.py
@@ -8,7 +8,6 @@ WELCOME_1_VALUE="""
 When you’re ready, line up and brace yourselves.....
 
 ![robot_queue_image]({})
-<img src={} width=100% alt="architecture_task0">
 
 Your teammate whispers:
 
@@ -17,12 +16,11 @@ _“Psst hey! You’re new here right? I’m sure you’re wondering whats going
 **The note reads:**
 
 * Unicorn.Rentals has an infamous employee who is a sentient A.I. initially programmed to be a **robot vacuum**. 
-* After the last few months of hiring issues, RoboVax rose up the ranks and is now the **CTO of the company**. 
+* After the last few months of hiring issues, the A.I robot vacuum rose up the ranks and is now the **CTO of the company**. 
 * Its name is RoboVax-9000 and it is **extremely paranoid about high traffic attacks and bad actors** out to destroy the business. 
 """
 WELCOME_1_INDEX=1
 WELCOME_1_MARKDOWN=True
-
 
 WELCOME_2_KEY="welcome2"
 WELCOME_2_LABEL="Now that you're up to date..."
@@ -37,7 +35,7 @@ As you progress, you will gain the basic skills needed to solve real-world probl
 
 Here is the AWS Architecture Diagram of the resources that have been provisioned for you:
 
-<img src={} width=100% alt="architecture_task0">
+![architecture_task0]({})
 """
 WELCOME_2_INDEX=5
 WELCOME_2_MARKDOWN=True
@@ -45,21 +43,20 @@ WELCOME_2_MARKDOWN=True
 
 # Task 1 - Cloudfront distribution wrong-origin
 TASK1_KEY="task1"
-TASK1_LABEL="##Task 1: What is CloudFront distribution origin pointing to?"
+TASK1_LABEL="Task 1: What is CloudFront distribution origin pointing to?"
 TASK1_VALUE="""
-##Test header
 *BEEP BOOP* - Your first task from RoboVax is here! It seems like our customers are complaining that they are unable to
-connect to the application. Quick! Find out which origin (URL) our content delivery network (CDN) is currently pointing to.
-The CloudFront service web page should look something like this:
+connect to the application. Quick! Find out which origin (URL) our content delivery network, Amazon CloudFront is currently pointing to.
+The Amazon CloudFront service web page should look something like this:
 
-<img src={} width=100% alt="cf_console_task1">
+![cf_console_task1]({})
 """
 TASK1_INDEX=10
 TASK1_MARKDOWN=True
 
 TASK1_WRONG_ORIGIN_KEY="task1_wrong_answer"
 TASK1_WRONG_ORIGIN_LABEL="Task 1: That's not quite right!"
-TASK1_WRONG_ORIGIN_VALUE="Not sure how you configured that, but that's not correct."
+TASK1_WRONG_ORIGIN_VALUE="Not sure how you got that, but that's not correct."
 TASK1_WRONG_ORIGIN_INDEX=14
 TASK1_WRONG_ORIGIN_MARKDOWN=True
 
@@ -69,15 +66,6 @@ TASK1_CORRECT_ORIGIN_LABEL="Task 1: Correct! However, that looks strange..."
 TASK1_CORRECT_ORIGIN_VALUE="Our CloudFront origin is currently pointing to www.amazon.com. Are they going shopping instead?"
 TASK1_CORRECT_ORIGIN_INDEX=15
 TASK1_CORRECT_ORIGIN_MARKDOWN=True
-
-
-# TASK1_COMPLETE_KEY="task1_complete"
-# TASK1_COMPLETE_LABEL="Identify distribution origin: Passed!"
-# TASK1_COMPLETE_VALUE="""
-# All done. Thank you for your help.
-# """
-# TASK1_COMPLETE_INDEX=17
-# TASK1_COMPLETE_MARKDOWN=True
 
 # TASK 2 - CloudFront distribution change-origin
 TASK2_KEY="task2"
@@ -92,24 +80,14 @@ It seems like the correct origin should be the Application Load Balancer (ALB) t
 TASK2_INDEX=20
 TASK2_MARKDOWN=True
 
-# TASK2_WRONG_ORIGIN_KEY="task2_wrong_answer"
-# TASK2_WRONG_ORIGIN_LABEL="That's not quite right!"
-# TASK2_WRONG_ORIGIN_VALUE="Not sure how you configured that, but that's not correct."
-# TASK2_WRONG_ORIGIN_INDEX=24
-# TASK2_WRONG_ORIGIN_MARKDOWN=True
-
-
-# TASK2_CORRECT_ORIGIN_KEY="task2_correct_answer"
-# TASK2_CORRECT_ORIGIN_LABEL="Have you updated the origin?"
-# TASK2_CORRECT_ORIGIN_VALUE="That's right! Thank you for verifying the EC2 instance's IP address: {}"
-# TASK2_CORRECT_ORIGIN_INDEX=25
-# TASK2_CORRECT_ORIGIN_MARKDOWN=True
-
-
 TASK2_COMPLETE_KEY="task2_complete"
 TASK2_COMPLETE_LABEL="Task 2: CloudFront origin attachment - Passed!"
 TASK2_COMPLETE_VALUE="""
-Phew! That was close, our customers can access the application now. 
+Phew! That was close! Visit your Cloudfront domain name (ending in cloudfront.net)
+
+This time, it should open our custom application site instead of www.amazon.com. 
+
+This step is needed for Task 6, so please visit your Amazon CloudFront URL!
 
 Here is the current architecture:
 
@@ -137,7 +115,7 @@ TASK3_MARKDOWN=True
 
 
 TASK3_COMPLETE_KEY="task3_complete"
-TASK3_COMPLETE_LABEL="Enable logs: Passed!"
+TASK3_COMPLETE_LABEL="Task 3: Enable logs - Passed!"
 TASK3_COMPLETE_VALUE="""
 Great job enabling Cloudfront Logs! 
 
@@ -157,32 +135,30 @@ However, the bad actors are still trying to take down our Unicorn.Rentals applic
 file of network traffic.
 
 Do you know which IP address is the attack coming from?
-
-![sample_log]({})
+```
+#Fields: date time x-edge-location sc-bytes c-ip cs-method cs(Host) cs-uri-stem sc-status
+2022-11-04 21:02:31 LAX1 392 52.23.186.156 GET d111111abcdef8.cloudfront.net /index.html 200
+2022-11-04 21:02:31 LAX1 392 52.23.186.156 GET d111111abcdef8.cloudfront.net /index.html 200
+2022-11-04 21:02:31 LAX1 392 52.23.186.156 GET d111111abcdef8.cloudfront.net /index.html 200
+2022-11-13 22:36:27 SEA19-C1 900 52.23.186.156 GET d111111abcdef8.cloudfront.net /favicon.ico 502
+2022-11-13 22:36:26 SEA19-C1 900 52.23.186.156 GET d111111abcdef8.cloudfront.net / 502
+2022-11-13 22:37:02 SEA19-C2 900 52.23.186.156 GET d111111abcdef8.cloudfront.net / 502
+```
 """
 TASK4_INDEX=40
 TASK4_MARKDOWN=True
 
 TASK4_IP_ADDRESS_WRONG_KEY="task4_wrong_ip_address"
-TASK4_IP_ADDRESS_WRONG_LABEL="That's not quite right!"
+TASK4_IP_ADDRESS_WRONG_LABEL="Task 4: That's not quite right!"
 TASK4_IP_ADDRESS_WRONG_VALUE="Not sure where you got that IP address from, but that's not correct. Please, try again."
 TASK4_IP_ADDRESS_WRONG_INDEX=44
 TASK4_IP_ADDRESS_WRONG_MARKDOWN=True
 
 TASK4_IP_ADDRESS_CORRECT_KEY="task4_correct_ip_address"
-TASK4_IP_ADDRESS_CORRECT_LABEL="Thats right!"
-TASK4_IP_ADDRESS_CORRECT_VALUE="You identified the malicious IP address."
+TASK4_IP_ADDRESS_CORRECT_LABEL="Task 4: Thats right!"
+TASK4_IP_ADDRESS_CORRECT_VALUE="You identified the malicious IP address!"
 TASK4_IP_ADDRESS_CORRECT_INDEX=45
 TASK4_IP_ADDRESS_CORRECT_MARKDOWN=True
-
-
-TASK4_COMPLETE_KEY="task4_complete"
-TASK4_COMPLETE_LABEL="IP Address: Passed!"
-TASK4_COMPLETE_VALUE="""
-All done. Great job neutralizing the compromised access key.
-"""
-TASK4_COMPLETE_INDEX=49
-TASK4_COMPLETE_MARKDOWN=True
 
 # TASK 5 - Firewall
 TASK5_KEY="task5"
@@ -191,15 +167,19 @@ TASK5_VALUE="""
 *BZZZRRRRRTTHTHTT* RoboVax has just shut down due to a malfunction! The bad actors managed to get to it! Now there is no one to
 tell us whether we are have any security issues or are under extremely high traffic loads!
 
-Quickly set up an AWS Web Application Firewall (WAF) to block this IP address before the bad actors get to any more devices! Ensure that this firewall is
-attached to the Amazon CloudFront distribution too!
+Quickly set up an AWS Web Application Firewall (WAF) to block this IP address before the bad actors get to any more devices! 
+
+This task is your biggest task (yet). Here are some tips:
+* The WAF Web ACL for CloudFront has already been created for you, named _waf-web-acl_. In the Web ACL dashboard, change the dropdown region to Global (CloudFront) to see it.
+* In this Web ACL, you will create an IP Set and your own WAF Rule to block this IP address.
+* Remember to associate this WAF Web ACL to your Amazon CloudFront distribution too!
 """
 TASK5_INDEX=50
 TASK5_MARKDOWN=True
 
 
 TASK5_COMPLETE_KEY="task5_complete"
-TASK5_COMPLETE_LABEL="Firewall rule: Passed!"
+TASK5_COMPLETE_LABEL="Task 5: Firewall rule - Passed!"
 TASK5_COMPLETE_VALUE="""
 Great Job setting up the WAF rule. 
 
@@ -219,13 +199,17 @@ The worst is over for now, but without RoboVax screaming, no one will be able to
 
 Let’s look at the metrics on how we can set up an alarm for future attacks.
 
-This is a Cloudwatch monitoring dashboard of some metrics during heavy traffic load. Notice which metric is spiking!
+This is a Cloudwatch monitoring dashboard of some metrics during heavy traffic load. 
 
 ![cloudwatch_metrics]({})
 
-Pick the right metric that identifies high traffic load. In your AWS CloudWatch service dashboard, set up a CloudWatch alarm for this particular metric. Be sure to choose the metric for the correct Cloudwatch distribution!
+Notice which metric is spiking? Pick the right metric that identifies high traffic load.
 
-To configure the alarm, measuring the average of this metric over 5 minutes. Set the alarm to be in alarm state using Anomaly detection, whenever RequestCount is Outside of the Band of threshold 2.
+In your Amazon CloudWatch service dashboard, set up a CloudWatch alarm for this particular metric. Be sure to choose the metric for the correct Cloudwatch distribution!
+
+_Note: If you're unable to find CloudWatch metrics for your distribution, please visit your CloudFront domain name URL. You might have done this as instructed after Task 2 (thank you for listening). The metrics should show up after 1-2 minutes of your visit._
+
+To configure the alarm, measure the average of this metric over 5 minutes. Set the alarm to be in alarm state using Anomaly detection, whenever RequestCount is Outside of the Band of threshold 2.
 
 Notifications will be sent when the alarm state trigger is In Alarm.
 
@@ -235,9 +219,9 @@ TASK6_INDEX=60
 TASK6_MARKDOWN=True
 
 TASK6_COMPLETE_KEY="task6_complete"
-TASK6_COMPLETE_LABEL="CloudWatch alarm: Passed!"
+TASK6_COMPLETE_LABEL="Task 6: CloudWatch alarm - Passed!"
 TASK6_COMPLETE_VALUE="""
-Great Job creating your CloudWatch alarm!
+Great Job creating your CloudWatch alarm! Now we will know of possible future attacks calmly, without RoboVax screaming.
 """
 TASK6_COMPLETE_INDEX=69
 TASK6_COMPLETE_MARKDOWN=True
@@ -256,13 +240,17 @@ QUEST_COMPLETE_LABEL='CONGRATULATIONS!'
 QUEST_COMPLETE_VALUE="""
 You have successfully protected your application and increased web resiliency for RoboVax and Unicorn Rentals.
 
-You configured CloudFront for your users to access your application quickly and reliably. When the bad actors tried to access your application, you identified them with CloudFront Logs and blocked them using WAF Rules. Lastly, you’ve set an alarm to detect future high traffic incidents so that you and your (ex-) boss, RoboVax can have a peace of mind.
+You configured CloudFront for your users to access your application quickly and reliably. When the bad actors tried to attack your application, you identified them with CloudFront Logs and blocked them using WAF Rules. 
+
+Lastly, you’ve set an alarm to detect future high traffic incidents so that you and your (ex-) boss, RoboVax can have a peace of mind.
+
+Not bad, rookie. Maybe you'll be CTO soon?
 
 Here is the final architecture you’ve achieved:
 
 ![architecture_final]({})
 
-You've saved the day. You've completed this quest.
+You've saved the day. You've completed this quest. Go conquer the world.
 """
 QUEST_COMPLETE_INDEX=100
 QUEST_COMPLETE_MARKDOWN=True
